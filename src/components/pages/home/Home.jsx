04172d9ac4f4a61e46a24b1c2e79dc241b777a6a
George <gwt774@hotmail.com>
2021-07-23 12:49:19 -0300

    Implementando navbar

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import * as S from './style';
 import logoSystem from '../../../assets/img/logo-system.png';
-
-
+import IconLogin from '../../../assets/img/account_circle_white_24dp.svg';
 
 const Home = () => {
 
     return (      
         <S.ContainerPrincipal>
             <S.MenuLateral>
-               <S.Logo src={logoSystem} />
-                
-                
-            </S.MenuLateral>            
+               <S.Logo src={logoSystem} />       
+            </S.MenuLateral>
+                <S.ContainerLateral>      
+                <S.Navbar>
+                    <S.LinkMenuNavbar>
+                        Login
+                    </S.LinkMenuNavbar>
+                    <S.IconAvatar src={IconLogin}/>                    
+                </S.Navbar>
+                <S.ContainerConteudo></S.ContainerConteudo>
+            </S.ContainerLateral>      
         </S.ContainerPrincipal>         
       
     )
